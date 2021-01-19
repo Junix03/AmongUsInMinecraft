feab3c4cb525c57bad5a074da8dd741171286afa
Junix03 <Uschner.Jeremy@gmail.com>
2021-01-19 10:32:46 +0100

    Initial Commit

---
+++ src/main/java/com/Junix03/AmongUs.java
@@ -0,0 +1,17 @@
+package com.Junix03;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AmongUs extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
