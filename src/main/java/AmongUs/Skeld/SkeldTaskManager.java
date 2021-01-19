b279825e6797b6a4a3309088563b972da3aa83cb
Junix03 <Uschner.Jeremy@gmail.com>
2021-01-19 15:53:32 +0100

    Add functioning plugin.yml

--- src/main/java/AmongUs/Skeld/SkeldTaskManager.java
+++ src/main/java/AmongUs/Skeld/SkeldTaskManager.java
@@ -11,6 +11,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Random;
 
