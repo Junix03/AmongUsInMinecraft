1d1a1fcf3edcb7d305adbee326d07580429ad320
Junix03 <Uschner.Jeremy@gmail.com>
2021-01-22 09:59:34 +0100

    Add Gamestart

--- src/main/java/AmongUs/Skeld/SkeldGameManager.java
+++ src/main/java/AmongUs/Skeld/SkeldGameManager.java
@@ -12,6 +12,10 @@ import com.destroystokyo.paper.event.server.ServerTickStartEvent;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarFlag;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.boss.BossBar;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.EntityType;
@@ -21,28 +25,32 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.EulerAngle;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Random;
 
 public class SkeldGameManager implements Listener {
 
     private final static String PREFIX = "§2[Among Us | The Skeld]: §r";
     public static SkeldGameManager INSTANCE;
 
-    private final ArrayList<Player> playerList = new ArrayList<>();
+    private ArrayList<Player> playerList = new ArrayList<>();
     private final ColorUtil colorUtil = new ColorUtil();
     private boolean isOpen = true;
     private final SkeldSettings settings = new SkeldSettings();
 
-    private final ArrayList<Player> crewmateList = new ArrayList<>();
-    private final ArrayList<Player> impostorList = new ArrayList<>();
-    private final ArrayList<SkeldTaskManager> taskmanagerList = new ArrayList<>();
+    private ArrayList<Player> crewmateList = new ArrayList<>();
+    private ArrayList<Player> impostorList = new ArrayList<>();
+    private ArrayList<SkeldTaskManager> taskmanagerList = new ArrayList<>();
 
-    private final ArrayList<Player> ghostList = new ArrayList<>();
-    private final SkeldImpostorManager impostorManager = new SkeldImpostorManager();
-    private final ArrayList<ArmorStand> corpsesList = new ArrayList<>();
+    private ArrayList<Player> ghostList = new ArrayList<>();
+    private SkeldImpostorManager impostorManager = new SkeldImpostorManager();
+    private ArrayList<ArmorStand> corpsesList = new ArrayList<>();
+    private BossBar taskProcessBar;
 
     private boolean isMeeting = false;
 
@@ -64,8 +72,81 @@ public class SkeldGameManager implements Listener {
             return;
         }
 
+        impostorManager = new SkeldImpostorManager();
+        Random random = new Random();
         isOpen = false;
         setLobbyEntrance(false);
+
+        while (impostorList.size() < settings.getImpostor())
+            impostorList.add(playerList.get(random.nextInt(playerList.size() - 1)));
+        ArrayList<Player> tempPlayerList = (ArrayList<Player>) playerList.clone();
+        tempPlayerList.removeAll(impostorList);
+        crewmateList = tempPlayerList;
+
+        Title crewmateTitle;
+        if (settings.getImpostor() == 1)
+            crewmateTitle = new Title("§bCREWMATE", "There is 1 impostor among us", 20, 60, 20);
+        else {
+            crewmateTitle = new Title("§bCREWMATE", "There are " + settings.getImpostor() + " impostors among us", 20, 60, 20);
+        }
+        String impostors = "";
+        for (Player player1: impostorList)
+            impostors += player1.getName() + "; ";
+        Title impostorTitle;
+        if (settings.getImpostor() == 1)
+            impostorTitle = new Title("§cIMPOSTOR", "You are Impostor", 20, 60, 20);
+        else
+            impostorTitle = new Title("§cIMPOSTOR", "The impostors are: " + impostors, 20, 60, 20);
+
+        // Common Tasks
+        ArrayList<String> tempCommonTasks = new ArrayList<>();
+        ArrayList<String> commonTasks = new ArrayList<>();
+        tempCommonTasks.add("Electrical: Fix Wiring");
+        tempCommonTasks.add("Storage: Fix Wiring");
+        tempCommonTasks.add("Admin: Fix Wiring");
+        tempCommonTasks.add("Navigation: Fix Wiring");
+        tempCommonTasks.add("Cafeteria: Fix Wiring");
+        tempCommonTasks.add("Security: Fix Wiring");
+        tempCommonTasks.add("Admin: Swipe Card");
+
+        for (int i = 0; i < settings.getLongTasks(); i++) {
+            String task = tempCommonTasks.get(random.nextInt(tempCommonTasks.size() - 1));
+            if (task.contains("Fix Wiring")) {
+                tempCommonTasks.remove("Electrical: Fix Wiring");
+                tempCommonTasks.remove("Storage: Fix Wiring");
+                tempCommonTasks.remove("Admin: Fix Wiring");
+                tempCommonTasks.remove("Navigation: Fix Wiring");
+                tempCommonTasks.remove("Cafeteria: Fix Wiring");
+                tempCommonTasks.remove("Security: Fix Wiring");
+            }
+            tempCommonTasks.remove(task);
+            commonTasks.add(task);
+        }
+
+        for (Player p: crewmateList)
+            taskmanagerList.add(new SkeldTaskManager(commonTasks, p));
+
+        PotionEffect potionEffect = new PotionEffect(PotionEffectType.BLINDNESS, 100, 1, false, false);
+        for (Player player1: playerList)
+            player1.addPotionEffect(potionEffect);
+
+        // TODO Location for Game Start
+        Location location = new Location(AmongUs.INSTANCE.getServer().getWorld("skeld"), 0, 0, 0, 0, 0);
+        for (Player player1: playerList)
+            player1.teleport(location);
+
+        for (Player player1: crewmateList)
+            player1.sendTitle(crewmateTitle);
+        for (Player player1: impostorList)
+            player1.sendTitle(impostorTitle);
+
+        taskProcessBar = AmongUs.INSTANCE.getServer().createBossBar("Tasks completed:", BarColor.GREEN, BarStyle.SOLID, BarFlag.DARKEN_SKY);
+        taskProcessBar.setProgress(0);
+        for (Player player1: playerList)
+            taskProcessBar.addPlayer(player1);
+
+        for (Player player1: playerList)
+            player1.setWalkSpeed((float) (0.2 * settings.getPlayerSpeed()));
     }
 
     public boolean isHost(Player player) {
@@ -122,17 +203,12 @@ public class SkeldGameManager implements Listener {
             livingImpostorList.removeAll(ghostList);
             int livingImpostorCount = livingImpostorList.size();
 
-            if ((livingCrewmateCount - livingImpostorCount) <= 0)
-                endGame(2);
-            else
-                endGame(1);
+            endGame((livingCrewmateCount - livingImpostorCount) > 0);
         }
     }
 
-    public void endGame(int winner) {
-        boolean crewmateWin = true;
-        if (winner == 2)
-            crewmateWin = false;
+    public void endGame(boolean CrewWin) {
+        // TODO LOCATION FOR GAME END AND GAME END ITSELF
     }
 
     public void addGhost(Player player) {
@@ -170,6 +246,17 @@ public class SkeldGameManager implements Listener {
             }
     }
 
+    public void proceedTask(Player player, String task) {
+        int totalTaskNumber = 0;
+        for (Player player1: crewmateList)
+            totalTaskNumber += settings.getCommonTasks() + settings.getLongTasks() + settings.getShortTasks();
+        int relativeTaskNumber = 0;
+        for (Player player1:crewmateList)
+            relativeTaskNumber += getTaskManager(player1).getFullTaskList().size();
+        taskProcessBar.setProgress((float) (totalTaskNumber - relativeTaskNumber) / totalTaskNumber);
+        getTaskManager(player).proceedTask(task);
+    }
+
     public boolean isCrewmate(Player player) {
         return crewmateList.contains(player);
     }
@@ -183,10 +270,6 @@ public class SkeldGameManager implements Listener {
     }
 
     public ArrayList<Player> getImpostor() { return impostorList; }
-
-    public boolean isOpen() {
-        return isOpen;
-    }
 
     public boolean isPlayer(Player player) {
         return playerList.contains(player);
@@ -255,14 +338,14 @@ public class SkeldGameManager implements Listener {
             int livingImpostorCount = livingImpostorList.size();
 
             if ((livingCrewmateCount - livingImpostorCount) <= 0)
-                endGame(2);
+                endGame(false);
 
             int count = 0;
             for (SkeldTaskManager taskManager: taskmanagerList)
                 count += taskManager.getFullTaskList().size();
 
             if (count == 0)
-                endGame(1);
+                endGame(true);
         }
     }
 }
